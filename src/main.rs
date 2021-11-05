0a44cdd3ea0a60813ce9a1ab4a69d3eab09e20b5
hyperocean <hyperstellar.ocean@gmail.com>
2021-11-05 17:01:04 +0300

    Added searching in file

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,6 @@ fn main() {
         process::exit(1);
     });
 
-    println!("Search for {}", config.query);
-    println!("In file {}", config.filename);
-
     if let Err(e) = minigrep::run(config) {
         println!("File read error: {}", e);
         process::exit(1);
