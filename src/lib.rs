0e39f388e6f5694842351bf09975eee1722f9070
Raik Bitters <raikbitters@gmail.com>
2021-11-29 17:52:30 +0300

    Used iterators

--- src/lib.rs
+++ src/lib.rs
@@ -9,13 +9,18 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new (args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments");
-        }
+    pub fn new (mut args: std::env::Args) -> Result<Config, &'static str> {
+        args.next();
+        
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Query string not received."),
+        };
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("File name not received."),
+        };
 
-        let query = args[1].clone();
-        let filename = args[2].clone();
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 
         Ok(Config { query, filename, case_sensitive })
@@ -23,28 +28,17 @@ impl Config {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut result = Vec::new();
-    
-    for line in contents.lines() {
-        if line.contains(query) {
-            result.push(line);
-        }
-    }
-
-    result
+    contents.lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
-pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let query = query.to_lowercase();
-    let mut result = Vec::new();
-
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            result.push(line);
-        }
-    }
-
-    result
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {   
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase()
+                .contains(&query.to_lowercase()))
+        .collect()
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,11 @@ use std::process;
 use minigrep::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Arguments parsing error: {}", err);
-        process::exit(1);
-    });
+    let config = Config::new(env::args())
+        .unwrap_or_else(|err| {
+            println!("Arguments parsing error: {}", err);
+            process::exit(1);
+        });
 
     if let Err(e) = minigrep::run(config) {
         println!("File read error: {}", e);
